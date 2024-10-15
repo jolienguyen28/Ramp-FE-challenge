fa2e3b194ddc4cfd0b3ca2142b38ec69fb54af60
jolienguyen28 <123792049+jolienguyen28@users.noreply.github.com>
2024-10-15 01:23:17 -0400

    Solution bug 7

--- src/components/Transactions/TransactionPane.tsx
+++ src/components/Transactions/TransactionPane.tsx
@@ -1,21 +1,23 @@
-import { useState } from "react"
-import { InputCheckbox } from "../InputCheckbox"
-import { TransactionPaneComponent } from "./types"
+import { useState } from "react";
+import { InputCheckbox } from "../InputCheckbox";
+import { TransactionPaneComponent } from "./types";
+import { useCustomFetch } from "../../hooks/useCustomFetch"; //solution bug7.1
 
 export const TransactionPane: TransactionPaneComponent = ({
   transaction,
   loading,
   setTransactionApproval: consumerSetTransactionApproval,
 }) => {
-  const [approved, setApproved] = useState(transaction.approved)
-
+  const [approved, setApproved] = useState(transaction.approved);
+  const { clearCache } = useCustomFetch(); //solution bug7.2
   return (
     <div className="RampPane">
       <div className="RampPane--content">
         <p className="RampText">{transaction.merchant} </p>
         <b>{moneyFormatter.format(transaction.amount)}</b>
         <p className="RampText--hushed RampText--s">
-          {transaction.employee.firstName} {transaction.employee.lastName} - {transaction.date}
+          {transaction.employee.firstName} {transaction.employee.lastName} -{" "}
+          {transaction.date}
         </p>
       </div>
       <InputCheckbox
@@ -26,16 +28,17 @@ export const TransactionPane: TransactionPaneComponent = ({
           await consumerSetTransactionApproval({
             transactionId: transaction.id,
             newValue,
-          })
-
-          setApproved(newValue)
+          });
+          //solution bug7.3
+          await clearCache();
+          setApproved(newValue);
         }}
       />
     </div>
-  )
-}
+  );
+};
 
 const moneyFormatter = new Intl.NumberFormat("en-US", {
   style: "currency",
   currency: "USD",
-})
+});
